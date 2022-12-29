2f6f47a2c5742983039bcc439d2d43cd245add4c
Peter Macdonald <macdonald.peter90@gmail.com>
2022-12-29 13:11:29 +0100

    Rust grrs
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![allow(unused)]
+
+use clap::Parser;
+use anyhow::{Context, Result};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()));
+
+    for line in content.expect("REASON").lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
